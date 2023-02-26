27ee3dc933b0d4cf19726dae393f3d4fc0c8c741
IlyaIsupov1711 <107679539+IlyaIsupov1711@users.noreply.github.com>
2023-02-26 10:45:06 +0300

    feat: add index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig)
+{
+
+  let b_confing = bracketsConfig.reduce
+  (
+    (acc, value) =>
+    {
+      acc[value[0]] = value[1];
+      acc[value[1]] = null;
+      return acc;
+    },
+    {}
+  );
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++)
+  {
+    let char = str[i];
+    if (b_confing[char] === null)
+    {
+      if (stack.pop() !== char)
+      {
+        return false;
+      }
+    }
+    else if (b_confing[char] !== undefined)
+    {
+      stack.push(b_confing[char]);
+    }
+  }
+
+  if (stack.length !== 0)
+  {
+    return false;
+  }
+
+  return true;
+};
